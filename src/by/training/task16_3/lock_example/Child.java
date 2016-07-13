645b8eb877b3ac6136e5000ecf031ca14b926f95
Vladislav Zavadski <vladislav.zavadski@gmail.com>
2016-07-14 01:49:58 +0300

    refactor

--- src/by/training/task16_3/lock_example/Child.java
+++ src/by/training/task16_3/lock_example/Child.java
@@ -1,5 +1,8 @@
 package by.training.task16_3.lock_example;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+
 /**
  * Created by vladislav on 13.07.16.
  */
@@ -17,12 +20,16 @@ public class Child implements Runnable {
     public void run() {
         try {
             for (int i = 0; i < 10; i++) {//каждый из детей хочет съехать с горки по 10 раз
-                if (!hill.comeDown(this)){
+                System.out.println("Ребенок "+name+" хочет съехать с горки.");
+                Lock lock = hill.getLock();//получаем блокировку для горки
+                if (lock.tryLock(100, TimeUnit.MILLISECONDS)){//пробуем получить доступ к горке в течение 100 миллисекунд, в противном случае ребенок идёт домой
+                    hill.comeDown(this);//непосредственно едем с горки
+                    System.out.println("Ребенок "+name+" спустился с горки.");
+                    lock.unlock();//сообщаем об окончании спуска, теперь кто-то другой может съехать с горки.
+                    Thread.sleep(75);//время на подъём на горку.
+                }else {
                     System.out.println("Ребенок "+name+" не дождался освобождения горки и ушел домой.");
                     return;
-                }else {
-                    System.out.println("Ребенок "+name+" спустился с горки.");
-                    Thread.sleep(75);//время на подъём на горку.
                 }
 
             }
--- src/by/training/task16_3/lock_example/Hill.java
+++ src/by/training/task16_3/lock_example/Hill.java
@@ -1,6 +1,5 @@
 package by.training.task16_3.lock_example;
 
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -10,16 +9,12 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Hill {
     private Lock lock = new ReentrantLock();
 
-    public boolean comeDown(Child child) throws InterruptedException {
-        System.out.println("Ребенок "+child.getName()+" хочет съехать с горки.");
-        if(lock.tryLock(100, TimeUnit.MILLISECONDS)){//каждый из детей ждёт 100 миллисекунд, чтобы спуститься с горки
-            System.out.println("Ребенок "+child.getName()+" едет с горки.");
-            Thread.sleep(50);//едем с горки
-            lock.unlock();//спустились с горки, и даём возможность кому-то еще спуститься с горки.
-            return true;
-        }else {
+    public void comeDown(Child child) throws InterruptedException {
+       System.out.println("Ребенок "+child.getName()+" едет с горки.");
+       Thread.sleep(50);//едем с горки
+    }
 
-            return false;
-        }
+    public Lock getLock(){
+        return lock;
     }
 }
